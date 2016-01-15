865deae4d326bac86e840b0f36f5ba55f554c2e9
Oakley Hall <ohall@walmart.com>
2016-01-15 08:26:50 -0800

    initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './src/hello';
+import Pong from 'react-pong';
+
+
+ReactDOM.render(
+  <Hello name="World" />,
+  document.getElementById('container')
+);
+
+ReactDOM.render(
+  <Pong/>,
+  document.getElementById('pong')
+);
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: 'dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
