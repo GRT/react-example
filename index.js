982a0fa45fd133cc9e14f230f4f13045647ec22b
ohall <ohall@grassroots-tech.com>
2016-01-18 10:37:36 -0500

    replace pong with updated version

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './src/hello';
+import Pong from 'react-pong';
 
 ReactDOM.render(
   <Hello name="World" />,
   document.getElementById('container')
+);
+ReactDOM.render(
+  <Pong/>,
+  document.getElementById('pong')
 );
